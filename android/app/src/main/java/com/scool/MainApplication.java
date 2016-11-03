909515fb1707a40d15c41b55f9eea2e75c0f1a84
Rasmus Eskola <fruitiex@gmail.com>
2016-11-03 14:40:11 +0200

    Rename Pepperoni app template to SCOOL

--- android/app/src/main/java/com/scool/MainApplication.java
+++ android/app/src/main/java/com/scool/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pepperoniapptemplate;
+package com.scool;
 
 import android.app.Application;
 import android.util.Log;
--- ios/SCOOLTests/SCOOLTests.m
+++ ios/SCOOLTests/SCOOLTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface SCOOLTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation SCOOLTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
